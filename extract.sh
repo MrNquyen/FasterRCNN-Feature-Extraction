1e3885cbbd64363daa2c897b787c5a13d81f72a1
npl <npl@uit-dgx01.cm.cluster>
2025-07-28 10:17:39 +0700

    modify file .sh

--- extract.sh
+++ extract.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-IMG_DIR="/data/npl/ViInfographicCaps/images/images"
-MODEL_FILE="/data/npl/ViInfographicCaps/workspace/feature_extraction/frcnn/detectron_model_2.pth"
-CONFIG_FILE="/data/npl/ViInfographicCaps/workspace/feature_extraction/frcnn/detectron_model_2.yaml"
+IMG_DIR="/data2/npl/ViInforgraphic/data/images/images"
+MODEL_FILE="/data2/npl/ViInforgraphic/workspace/feature_extraction/frcnn/detectron_model_2.pth"
+CONFIG_FILE="/data2/npl/ViInforgraphic/workspace/feature_extraction/frcnn/detectron_model_2.yaml"
 BATCH_SIZE=64
 NUM_FEATURES=100
-OUTPUT_FOLDER="/data/npl/ViInfographicCaps/features/faster_rcnn"
+OUTPUT_FOLDER="/data2/npl/ViInfographic/data/faster_rcnn"
 FEATURE_NAME="fc6"
 CONF_THRESH=0.5
 DEVICE="cuda:5"
 
-python /data/npl/ViInfographicCaps/workspace/feature_extraction/frcnn/extract.py --model_file "$MODEL_FILE" --config_file "$CONFIG_FILE" --batch_size $BATCH_SIZE --num_features $NUM_FEATURES --output_folder "$OUTPUT_FOLDER" --image_dir "$IMG_DIR" --feature_name $FEATURE_NAME --confidence_threshold $CONF_THRESH --device $DEVICE --background
+python /data2/npl/ViInforgraphic/workspace/feature_extraction/frcnn/FasterRCNN-Feature-Extraction/extract_features.py --model_file "$MODEL_FILE" --config_file "$CONFIG_FILE" --batch_size $BATCH_SIZE --num_features $NUM_FEATURES --output_folder "$OUTPUT_FOLDER" --image_dir "$IMG_DIR" --feature_name $FEATURE_NAME --confidence_threshold $CONF_THRESH --device $DEVICE --background
 
