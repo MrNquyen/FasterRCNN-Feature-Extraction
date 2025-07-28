3155fc7a956104e6e99ed0d803cb7a624c441b18
nguyentran4804 <khoinguyen4804@gmail.com>
2025-07-28 11:17:44 +0700

    modify extract_features.py

--- extract.sh
+++ extract.sh
@@ -8,7 +8,7 @@ NUM_FEATURES=100
 OUTPUT_FOLDER="/data2/npl/ViInfographic/data/faster_rcnn"
 FEATURE_NAME="fc6"
 CONF_THRESH=0.5
-DEVICE="cuda:5"
+DEVICE="cuda:2"
 
 python /data2/npl/ViInforgraphic/workspace/feature_extraction/frcnn/FasterRCNN-Feature-Extraction/extract_features.py --model_file "$MODEL_FILE" --config_file "$CONFIG_FILE" --batch_size $BATCH_SIZE --num_features $NUM_FEATURES --output_folder "$OUTPUT_FOLDER" --image_dir "$IMG_DIR" --feature_name $FEATURE_NAME --confidence_threshold $CONF_THRESH --device $DEVICE --background 
 
